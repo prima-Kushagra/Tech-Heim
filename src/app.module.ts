fbabbcfcdb0df24ad99cae69c7f72e291ed0b8df
prima-Kushagra <kushagra.saxena@primathon.in>
2026-03-16 16:37:36 +0530

    Build the tech-heim backend folder structure

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { CartsService } from './carts/carts.service';
+import { CartService } from './cart/cart.service';
+import { CartController } from './cart/cart.controller';
+import { CartModule } from './cart/cart.module';
+import { OrdersService } from './orders/orders.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { ReviewsModule } from './reviews/reviews.module';
+
+@Module({
+  imports: [AuthModule, ProductsModule, CartModule, OrdersModule, CategoriesModule, WishlistModule, ReviewsModule],
+  controllers: [AppController, CartController, OrdersController],
+  providers: [AppService, CartsService, CartService, OrdersService],
+})
+export class AppModule {}
